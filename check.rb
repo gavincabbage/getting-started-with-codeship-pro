a8f2c779483c051c031f6303a5a9ebc0a14dfc4e
Gavin Cabbage <gavincabbage@gmail.com>
2019-01-10 11:29:04 -0500

    test

--- check.rb
+++ check.rb
@@ -1,18 +1,28 @@
-require "redis"
-require "pg"
+# require "redis"
+# require "pg"
 
-def exit_if_not(expected, current)
-  puts "Expected: #{expected}"
-  puts "Current: #{current}"
-  exit(1) if expected != current
-end
+# def exit_if_not(expected, current)
+#   puts "Expected: #{expected}"
+#   puts "Current: #{current}"
+#   exit(1) if expected != current
+# end
 
-puts "Redis"
-redis = Redis.new(host: "redis")
-puts "REDIS VERSION: #{redis.info["redis_version"]}"
+# puts "Redis"
+# redis = Redis.new(host: "redis")
+# puts "REDIS VERSION: #{redis.info["redis_version"]}"
 
-sleep 4
-postgres_username = "postgres"
-postgres_password = ""
-test = PG.connect("postgres", 5432, "", "", "postgres", postgres_username, postgres_password)
-puts test.exec("SELECT version();").first["version"]
+# sleep 4
+# postgres_username = "postgres"
+# postgres_password = ""
+# test = PG.connect("postgres", 5432, "", "", "postgres", postgres_username, postgres_password)
+# puts test.exec("SELECT version();").first["version"]
+
+# puts ">>>>>>>>>>>>>>>>>>>>"
+# puts ENV['TEST_TOKEN']
+# puts ENV['TEST_GETTING_DEFAULTS']
+# puts ENV['CI_TIMESTAMP']
+# puts "<<<<<<<<<<<<<<<<<"
+
+puts "Running..."
+sleep 5
+puts "Done!"
